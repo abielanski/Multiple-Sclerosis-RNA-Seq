cf3d9553f46275144abd13c45eafa10e838246cf
zhasan11 <zarah.hasan11@gmail.com>
2023-05-05 01:16:14 -0500

    Folder containing all scripts

--- Final_Code/Kallisto/Final_Kallisto-Script.py
+++ Final_Code/Kallisto/Final_Kallisto-Script.py
@@ -7,10 +7,12 @@ from Bio import Entrez
 os.system("mkdir Multiple_Sclerosis_Kallisto")
 os.chdir("Multiple_Sclerosis_Kallisto")
 
+
 # Generate a log file
 log_file = open("Multiple_Sclerosis_Kallisto.log", "w")
 log_file.write("Log file generated.")
 log_file.close()
+
 
 # Retrive the reference transcriptome, Transgenic Mice, from Ensembl
 os.system("wget https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/cdna/Mus_musculus.GRCm39.cdna.all.fa.gz")
@@ -22,25 +24,25 @@ Chen_data = "/home/data/Chen_data"
 # NOTE: users will have to enter their Loyola UNIVID used to access the class server 
 Ref_T_path = "/home/<yourUNIVID>/Multiple_Sclerosis_Kallisto"
 
-# Create the Index 
+# Indexing command 
 os.system(f"time kallisto index -i MS.idx Mus_musculus.GRCm39.cdna.all.fa.gz")
 
 # Set the index path to be used later in the Kallisto quantification step
 index_path = "MS.idx"
 
-# Define the sample groups (from Dr. Chen's data) and their corresponding FASTQ files
+# Define the sample groups and their corresponding FASTQ files
 # Each tuple represents a sample with the following structure:
 # (sample_name, fastq_file)
 samples = [
-  ("CFA_1", "YC-C1_S5_L004_R1_001.fastq.gz"),
-  ("CFA_2", "YC-C2_S6_L004_R1_001.fastq.gz"),
-  ("CFA_3", "YC-C3_S7_L004_R1_001.fastq.gz"),
-  ("EAE_Vehicle_1", "YC-V1_S1_L004_R1_001.fastq.gz"),
-  ("EAE_Vehicle_2", "YC-V2_S2_L004_R1_001.fastq.gz"),
-  ("EAE_Vehicle_3", "YC-V3_S3_L004_R1_001.fastq.gz"),
-  ("EAE_Sephin1_1", "YC-S3_S4_L004_R1_001.fastq.gz"),
-  ("EAE_Sephin1_2", "YC-4_S8_L004_R1_001.fastq.gz"),
-  ("EAE_Sephin1_3", "YC-5_S9_L004_R1_001.fastq.gz"),
+    ("CFA_1", "YC-C1_S5_L004_R1_001.fastq.gz"),
+    ("CFA_2", "YC-C2_S6_L004_R1_001.fastq.gz"),
+    ("CFA_3", "YC-C3_S7_L004_R1_001.fastq.gz"),
+    ("EAE_Vehicle_1", "YC-V1_S1_L004_R1_001.fastq.gz"),
+    ("EAE_Vehicle_2", "YC-V2_S2_L004_R1_001.fastq.gz"),
+    ("EAE_Vehicle_3", "YC-V3_S3_L004_R1_001.fastq.gz"),
+    ("EAE_Sephin1_1", "YC-S3_S4_L004_R1_001.fastq.gz"),
+    ("EAE_Sephin1_2", "YC-4_S8_L004_R1_001.fastq.gz"),
+    ("EAE_Sephin1_3", "YC-5_S9_L004_R1_001.fastq.gz"),
 ]
 
 # Set the Chen data path
@@ -57,5 +59,6 @@ for sample, fastq_file in samples:
 os.system(f"kallisto quant -i {index_path} -o {output_folder}/{sample} --single -l 200 -s 20 -b 100 -t 4 {Chen_data}/{fastq_file}")
 
 # The next step is to run Sleuth on the kallisto output files in an R script.
+
 
 
--- Final_Code/Sleuth/Final_Sleuth-Script.R
+++ Final_Code/Sleuth/Final_Sleuth-Script.R
@@ -1,16 +1,32 @@
-# Load the required libraries
+# Install and Load the required libraries
+
+# Devtools - package to help run R script 
+install.packages("devtools")
+devtools::install_github("pachterlab/sleuth")
 library(sleuth)
+
+# Dplyr - package for data manipulation
+install.packages("dplyr")
 library(dplyr)
 
-# Read in the table describing samples and Kallisto output paths
-  # The table is located as table.csv under the "Sleuth" folder in GitHub - 
-  # follow the protocol listed in the "Input Data" section of the READ.me 
-stab <- read.csv("sleuthTable.csv", header = TRUE, stringsAsFactors = FALSE, sep = ",")
+# Biomart - a package that provides an interface to BioMart databases (e.g., Ensembl).
+install.packages("biomaRt")
+library(biomaRt)
+
+# install.packages("ggplot2")
+# a powerful package for creating static, interactive, and animated graphics
+library(ggplot2)
+
+# Read in the table describing samples and Kallisto output paths   
+stab <- read.csv("table.csv", header = TRUE, stringsAsFactors = FALSE, sep = ",")
+
+# Create a unique list of conditions from the 'condition' column of your input data
+unique_conditions <- unique(stab$condition)
 
 # Initialize the sleuth object using the sleuth_prep function
 so <- sleuth_prep(stab)
 
-# Perform differential expression analysis comparing the two conditions
+# Perform differential expression analysis comparing the conditions
 so <- sleuth_fit(so, ~condition, 'full')
 
 # Fit the reduced model to compare in the likelihood ratio test
@@ -20,39 +36,125 @@ so <- sleuth_fit(so, ~1, 'reduced')
 so <- sleuth_lrt(so, 'reduced', 'full')
 
 # Extract the test results from the sleuth object
-results_table <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
+results_table_lrt <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
 
-# Filter most significant results (FDR/qval < 0.05) and sort by pval
-significant_results <- dplyr::filter(results_table, pval <= 0.05) %>% dplyr::arrange(pval)
+# Initialize an empty list to store Wald test results
+wald_test_results <- list()
 
-# Print top 10 transcripts
-head(significant_results, n = 10)
+# Iterate through all pairs of conditions and perform the Wald test
+for (i in 1:(length(unique_conditions) - 1)) {
+  for (j in (i + 1):length(unique_conditions)) {
+    condition1 <- unique_conditions[i]
+    condition2 <- unique_conditions[j]
+    
+    # Filter samples for the current pair of conditions
+    stab_filtered <- stab[stab$condition %in% c(condition1, condition2),]
+    
+    # Re-run sleuth_prep, sleuth_fit, and Wald test for the current pair of conditions
+    so_filtered <- sleuth_prep(stab_filtered)
+    so_filtered <- sleuth_fit(so_filtered, ~condition, 'full')
+    
+    # Perform the Wald test and store the results in the wald_test_results list
+    test_name <- paste("wt", condition1, "vs", condition2, sep = "_")
+    so_filtered <- sleuth_wt(so_filtered, which_beta = paste0("condition", condition2), 'full')
+    results_table_wt <- sleuth_results(so_filtered, paste0("condition", condition2), 'wt', show_all = FALSE)
+    significant_results_wt <- dplyr::filter(results_table_wt, pval <= 0.05) %>% dplyr::arrange(pval)
+    wald_test_results[[test_name]] <- significant_results_wt
+  }
+}
 
-#write FDR < 0.05 transcripts to file
-write.table(significant_results, file="fdr05_results.txt",quote = FALSE,row.names = FALSE)
+# Process and save the results for LRT test comparisons
+for (test_name in names(list(lrt = results_table_lrt))) {
+  results <- list(lrt = results_table_lrt)[[test_name]]
+  
+  # Write FDR < 0.05 transcripts to file
+  output_file <- paste0("fdr05_results_", test_name, ".txt")
+  write.table(results, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+}
 
-# Get the IDs of top transcripts
-top_transcripts <- head(select(significant_results, target_id, pval, qval), n = 10)
+# Process and save the results for Wald test comparisons
+for (test_name in names(wald_test_results)) {
+  results <- wald_test_results[[test_name]]
+  
+  # Write FDR < 0.05 transcripts to file
+  output_file <- paste0("fdr05_results_", test_name, ".txt")
+  write.table(results, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  # Read the top transcripts from the saved txt file
+  txt_file <- output_file
+  fdr05_results <- read.table(txt_file, header = TRUE, sep = "\t")
+  
+  # Get the Ensembl transcript IDs from the txt file
+  transcript_ids <- fdr05_results$target_id
+  
+  # Set up the BioMart object for the organism of interest (e.g., Mus musculus)
+  mart <- useMart(biomart = "ensembl", dataset = "mmusculus_gene_ensembl")
+  
+  # Convert Ensembl transcript IDs to Ensembl gene IDs
+  ensembl_transcript_ids <- transcript_ids
+  
+  t2g <- getBM(filters = "ensembl_transcript_id_version",
+               attributes = c("ensembl_transcript_id_version", "ensembl_gene_id",'external_gene_name'),
+               values = ensembl_transcript_ids,
+               mart = mart)
+  
+  # Merge the gene IDs with the transcript IDs and p-values from the txt file
+  transcript_ids_gene_ids_pvals <- merge(fdr05_results, t2g, by.x = "target_id", by.y = "ensembl_transcript_id_version")
+  
+  # Calculate the score and create a new data frame with gene_id and score columns
+  rnk <- data.frame(gene_id = transcript_ids_gene_ids_pvals$ensembl_gene_id,
+                    score = -log10(transcript_ids_gene_ids_pvals$pval))
+  
+  # Remove any rows with missing gene IDs or scores
+  rnk <- na.omit(rnk)
+  
+  rnk_file <- paste0("gene_ids_with_scores_", test_name, ".rnk")
+  write.table(rnk, file = rnk_file, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
+  
+  # Ensure there are no spaces around the tab delimiter
+  rnk_content <- readLines(rnk_file)
+  rnk_content <- gsub("\\s+\\t|\\t\\s+", "\t", rnk_content)
+  writeLines(rnk_content, rnk_file)
+}
 
-# Extract needed results from Kallisto for plotting
-so <- sleuth_prep(stab, extra_bootstrap_summary = TRUE, read_bootstrap_tpm = TRUE)
-
-# Create a ranked gene list using Ensembl gene IDs
-rnk <- data.frame(gene_ids = significant_results$target_id,
-                  rank = seq_along(significant_results$target_id))
-
-# Save the ranked gene list as a tab-separated file for WebGestalt
-write.table(rnk, file = "gene_ids.rnk", sep = "\t", quote = FALSE, row.names = FALSE)
 
 # Use the first target_id from top_transcripts for plotting
-if (nrow(top_transcripts) > 0) {
-  top_target_id <- top_transcripts$target_id[1]
-  topplot <- plot_bootstrap(so, top_target_id, units = "tpm", color_by = "condition")
+
+# Create PCA plot
+pca_plot <- plot_pca(so, color_by = "condition", text_labels = FALSE) +
+  theme_bw() +
+  labs(title = "PCA Plot")
+
+# Save PCA plot as a .png file
+ggsave("pca_plot.png", plot = pca_plot, width = 7, height = 5, dpi = 300)
+
+# Create bootstrap plot
+so <- sleuth_prep(stab, extra_bootstrap_summary = TRUE, read_bootstrap_tpm = TRUE)
+
+# Extract the top transcript ID from the significant_results data frame
+top_transcript_id <- significant_results_lrt$target_id[1]
+
+# Call plot_bootstrap function
+bootstrap_plot <- plot_bootstrap(so, top_transcript_id, units = "tpm", color_by = "condition")
+
+# Save plot as PNG file
+png(filename = paste0(top_transcript_id, "_TPM_bootstrap_plot.png"))
+print(bootstrap_plot)
+dev.off()
+
+# Create a volcano plot using the significant results of the Wald test for each comparison
+for (test_name in c("wt_CFA_vs_EAE_Sephin1", "wt_CFA_vs_EAE_Vehicle", "wt_EAE_Sephin1_vs_EAE_Vehicle")) {
+  volcano_data <- combined_results[[test_name]] %>%
+    dplyr::mutate(neg_log10_pval = -log10(pval)) %>%
+    dplyr::select(target_id, b, neg_log10_pval)
   
-  # Save plot as PNG file
-  png(paste0(top_target_id, "_TPM.png"))
-  plot(topplot)
-  dev.off()
-} else {
-  cat("No significant transcripts found.\n")
+  volcano_plot <- ggplot(volcano_data, aes(x = b, y = neg_log10_pval)) +
+    geom_point(alpha = 0.5, size = 1) +
+    theme_bw() +
+    labs(title = paste0("Volcano Plot: ", test_name),
+         x = "Log2 Fold Change",
+         y = "-Log10 P-value")
+  
+  # Save volcano plot as a .png file
+  ggsave(paste0("volcano_plot_", test_name, ".png"), plot = volcano_plot, width = 7, height = 5, dpi = 300)
 }
